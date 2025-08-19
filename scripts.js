91fd10a3d5684cc1f6a600b109d5af5ada7e1435
Wojciech Chrząszcz <wchrzaszcz@sii.pl>
2025-08-19 16:50:18 +0200

    3rd

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const commentBtn = document.getElementById("commentBtn");
+  const messageArea = document.getElementById("message-area");
+
+  commentBtn.addEventListener("click", () => {
+    // Clear old message if exists
+    messageArea.innerHTML = "";
+
+    // Create a new text field
+    const field = document.createElement("div");
+    field.className = "message-box";
+    field.textContent = "You clicked comment button";
+
+    // Append below buttons
+    messageArea.appendChild(field);
+  });
+});
